8f463e60c4b31427c70442f830fa8f2bcf7d8400
tarunbamba <tarunbamba@gmail.com>
2019-07-02 14:44:42 +0530

    sends the bulk email through Python, CSV and txt files
    This is a small application which sends the email (Mail text in any .txt file) to many users (users email can stored in the .csv file. The project can uses as a bulk email service. The code is written in python and uses 2 classes SMTP and CSV

---
+++ bulkemail.py
@@ -0,0 +1,21 @@
+﻿import smtplib
+import csv
+yourmailaddress = 'your email id here' 
+yourmailpassword = 'your email password here'     
+with open('testdata.csv', 'r') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            ' row[4]  is the row where the email id exist in the csv
+            mailto = row[4]
+            with open('anyfilename.txt', 'r') as myfile:
+                msg = myfile.read()
+                ' you can use any mail server smtp and password
+            mailServer = smtplib.SMTP('Smtp.gmail.com' , 587)
+            mailServer.starttls()
+            mailServer.login(yourmailaddress , yourmailpassword)
+            mailServer.sendmail(yourmailaddress, mailto , msg)
+            ' This will print the email id where the email send
+            ' read data from your csv file.
+            print("Mail sent to ",row[4])
+print(" \n Email Sent complete... Thanks for using the Service")
+mailServer.quit()
